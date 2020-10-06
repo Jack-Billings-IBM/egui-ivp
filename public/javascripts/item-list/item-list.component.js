8f4219e2d6be62c85490c2acbc0314e0254d21e3
Jack-Billings-IBM <44097415+Jack-Billings-IBM@users.noreply.github.com>
2020-10-06 12:44:03 -0500

    Update item-list.component.js

--- public/javascripts/item-list/item-list.component.js
+++ public/javascripts/item-list/item-list.component.js
@@ -9,12 +9,7 @@ angular.
       var self = this;
       self.orderProp = 'ca_description';
 
-      $http.get('http://esysmvs.wsclab.washington.ibm.com:39555/catalogManager/items?startItemID=0010',
-	       {
-          		headers: {Authorization: 'Basic amJpbGw6dGhvcjEz'},
-        	}          
-      	)
-	       .then(function(response) {
+      $http.get('http://esysmvs.wsclab.washington.ibm.com:39555/catalogManager/items?startItemID=0010').then(function(response) {
 
 			self.items = response.data.DFH0XCMNOperationResponse.ca_inquire_request.ca_cat_item;
 	
